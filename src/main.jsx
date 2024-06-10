ec0e73013c31ee4428da80e785b63c7db2df07f4
maria <mafeanaya22@gmail.com>
2024-06-09 22:08:31 -0500

    mafe

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import Componente from './nosocomponents'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
+    <Componente/>
   </React.StrictMode>,
+
 )
---
+++ src/nosocomponents.jsx
@@ -0,0 +1,9 @@
+import './main'
+export default function Componente(){
+    return(
+        <header className='flex items-center justify-center min-h-screen '>
+        <h1 className= 'border border-red-500' > Sobre nosotros </h1>
+        <p className='border border-red-500 flex flex-col justify-center' >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis  <br/> modi consequatur quasi consequuntur mollitia architecto illum? <br /> Ipsa impedit aliquam at excepturi quo dolorum minima quae omnis, voluptatibus ab eligendi cupiditate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ab praesentium beatae nesciunt! Doloribus iste voluptates quo totam omnis laudantium quisquam deleniti possimus sunt. Labore vel repudiandae inventore minus autem! </p>
+        </header>
+    ) 
+}
