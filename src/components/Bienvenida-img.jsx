4f0b5a15d6eb92cbaab89c43cd289ba105d679b3
kath <vasquezdianakatherine@gmailcom>
2024-06-13 11:58:34 -0500

    tercer cambio

--- src/components/Bienvenida-img.jsx
+++ src/components/Bienvenida-img.jsx
@@ -1,16 +1,16 @@
 import React from "react"
-import celularImg from '../images/img-celular.png';
-
 
 export default function imgFondo() {
     return (
         <>
       {/* Contenedor de la imagen y texto */}
-      <section>
-        <img src={celularImg} alt="Imagen de fondo" />
+      <section className="w-[70%] h-[75%] ml-52 flex static mt-0">
+        <img className="w-[100%] -mt-4 " src="src/images/img-celular.png" alt="imagen" />
+        <article className=" h-[25%] p-2 ml-12 mt-40 font-semibold text-3xl absolute text-white ">
         <h1>Manos a la obra</h1>
         <p>#YoCompostoYRecicloyTu</p>
         <p>#MisReciduosNoSonBasura</p>
+        </article>
       </section>
     </>
     )
--- src/components/Header-logo.jsx
+++ src/components/Header-logo.jsx
@@ -1,28 +1,35 @@
 import React from "react"
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 export default function Header() {
     return (
         <>
-        {/* Encabezado*/}
-        <header className="w-full h-[20%] flex justify-around bg-blue-200">
-            {/* contenedor del logo y texto*/}
-            <section className="w-80 bg-blue-400">
-                <img src="src/images/logoArogNuevo.png" alt="AROG"/>
-                <p>Asociación de recicladores de oficio GOLEROS </p>
-            </section>
-            <section>
-                <nav>
-                    <ul>
-                        <li><a href="#">Servicios</a></li>
-                        <li><a href="#">Nosotros</a></li>
-                        <li><a href="#">Iniciar sesión</a></li>
-                        <li><a href="#">Crear cuenta</a></li>
-                    </ul>
-                </nav>
-            </section>
-        </header>
-       
+            {/* Encabezado*/}
+            <header className="w-full h-[14%] flex mt-3">
+                {/* contenedor de logo y texto*/}
+                <section className="flex w-[35%]">
+                    {/* contenedor del logo*/}
+                    <article className="w-[19%] ml-24">
+                        <img className="w-[87%] object-fit " src="src/images/logoArogNuevo.png" alt="AROG" />
+                    </article>
+                    {/* contenedor del texto*/}
+                    <article className="w-[45%] pl-2 pt-2">
+                        <p className="font-semibold text-[20px] text-left leading-5">Asociación de recicladores de oficio <span class=" text-[35px] font-bold">GOLEROS </span></p>
+                    </article>
+                </section>
+                {/* contenedor del menu*/}
+                    <nav className=" w-[65%] flex  justify-center">
+                        <ul className="flex w-full pt-10 font-semibold text-xl">
+                            <li className="ml-12 mr-2"><a href="#">Servicios <span className="p-2">|</span></a></li>
+                            <li className=""><a href="#">Nosotros</a></li>
+                            <li className=" ml-64"><a href="#">Iniciar sesión <span className="p-2">|</span></a></li>
+                            <li><a href="#">Crear cuenta</a></li>
+                        </ul>
+                    </nav>
+                
+            </header>
+
         </>
     )
 }
