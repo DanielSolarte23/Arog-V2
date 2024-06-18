c3d19aadee9300fcf6eeb384a945508708bdcfcc
DanielSolarte23 <royersolarte22@gmail.com>
2024-06-18 16:26:38 -0500

    main

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Registro from './pages/Registro-Page'
 
-
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <Registro />
