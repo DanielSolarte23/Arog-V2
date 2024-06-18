7a476b3aee3ad00b6da434ba449c4f1952260fde
JuanVal092002 <juancarlospastasvalencia@hotmail.com>
2024-06-18 18:00:40 -0500

    primer cambio

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import Servicios from './pages/servicios-page'
+
 
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  </React.StrictMode>,
+    <Servicios/>
+    </React.StrictMode>,
 )
--- src/pages/PPComponent.jsx
+++ src/pages/PPComponent.jsx
@@ -23,6 +23,7 @@ export default function PPage() {
                 </div>
             </header>
             <BarraNav />
+            
         </main>
     )
 }
