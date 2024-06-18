ee4f02ac773f3ee0d59e4cae1fa60b15c4c97206
kathe <vasquezdianakatherine@gmail.com>
2024-06-18 18:17:36 -0500

    icons

--- src/components/Bienvenida-img.jsx
+++ src/components/Bienvenida-img.jsx
@@ -4,9 +4,9 @@ export default function imgFondo() {
     return (
         <>
       {/* Contenedor de la imagen y texto */}
-      <section className="w-[70%] h-[75%] ml-52 flex static mt-0">
-        <img className="w-[100%] -mt-4 " src="src/images/img-celular.png" alt="imagen" />
-        <article className=" h-[25%] p-2 ml-12 mt-40 font-semibold text-3xl absolute text-white ">
+      <section className="w-[70%] h-[70%] ml-52 flex static mt-0 rounded-2xl bg-cover bg-center  bg-no-repeat bg-[url('src/images/textura-hoja-verde.jpg')] bg-slate-500">
+        <img className=" ml-[45%]" src="src/images/gif-cell-phone.gif" alt="cell phone" />
+        <article className=" h-[25%] p-2 ml-12 mt-64 font-semibold text-5xl absolute text-white ">
         <h1>Manos a la obra</h1>
         <p>#YoCompostoYRecicloyTu</p>
         <p>#MisReciduosNoSonBasura</p>
