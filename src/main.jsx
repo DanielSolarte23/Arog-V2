71fd4b7cf30bbc8671286d872997dd887f6db60c
DanielSolarte23 <royersolarte22@gmail.com>
2024-06-18 16:15:23 -0500

    carpetas

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Registro from './pages/Registro-Page'
-
-
+import App from './App'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <Registro />
+    <App />
   </React.StrictMode>,
 )
