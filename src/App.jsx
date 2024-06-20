e65f172e4c1afc5a2ee1119d8cc228d49378003b
DanielSolarte23 <royersolarte22@gmail.com>
2024-06-20 17:44:54 -0500

    CambiosApp

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import React from "react"
-import SobreNosotros from "./components/publicas/SobreNosotrosComponente";
-import Regisincidencia  from "./components/admins/Regisincidencia";
+
 export default function App(){
     return(
         <>
-        <SobreNosotros/>
-        <Regisincidencia/>
         </>
     );
 };
