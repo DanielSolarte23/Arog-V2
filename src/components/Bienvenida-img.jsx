4c55bcff2fc73ecb63bd25f8177d729cfe6af9cf
kathe <Vasquezdianakatherine@mail.com>
2024-06-20 11:31:57 -0500

    responsive

--- src/components/Bienvenida-img.jsx
+++ src/components/Bienvenida-img.jsx
@@ -4,9 +4,9 @@ export default function imgFondo() {
     return (
         <>
       {/* Contenedor de la imagen y texto */}
-      <section className="w-[70%] h-[70%] ml-52 flex static mt-0 rounded-2xl bg-cover bg-center  bg-no-repeat bg-[url('src/images/textura-hoja-verde.jpg')] bg-slate-500">
-        <img className=" ml-[45%]" src="src/images/gif-cell-phone.gif" alt="cell phone" />
-        <article className=" h-[25%] p-2 ml-12 mt-64 font-semibold text-5xl absolute text-white ">
+      <section className="sm: w-[90%] h-[45%] ml-7 flex static mt-24 rounded-2xl bg-cover bg-center  bg-no-repeat bg-[url('src/images/textura-hoja-verde.jpg')]">
+        <img className="sm: ml-[32%] mt-[-10%] absolute w-[95%] " src="src/images/gif-cell-phone.gif" alt="cell phone" />
+        <article className="sm: h-[25%] p-2 ml-5 mt-28 font-semibold text-3xl absolute text-white ">
         <h1>Manos a la obra</h1>
         <p>#YoCompostoYRecicloyTu</p>
         <p>#MisReciduosNoSonBasura</p>
--- src/components/Header-logo.jsx
+++ src/components/Header-logo.jsx
@@ -6,30 +6,34 @@ export default function Header() {
     return (
         <>
             {/* Encabezado*/}
-            <header className="w-full h-[14%] flex mt-3">
+            <header className=" sm:bg-slate-200 w-screen h-[14%] flex mt-2 ">
                 {/* contenedor de logo y texto*/}
-                <section className="flex w-[35%]">
+                <section className="sm: flex w-[70%] bg-slate-500">
                     {/* contenedor del logo*/}
-                    <article className="w-[19%] ml-24">
-                        <img className="w-[87%] object-fit " src="src/images/logoArogNuevo.png" alt="AROG" />
+                    <article className="sm: w-[40%] ml-3 bg-slate-400">
+                        <img className="sm: w-[80%] ml-3 object-fit " src="src/images/logoArogNuevo.png" alt="AROG" />
                     </article>
                     {/* contenedor del texto*/}
-                    <article className="w-[45%] pl-2 pt-2">
-                        <p className="font-semibold text-[20px] text-left leading-5">Asociación de recicladores de oficio <span class=" text-[35px] font-bold">GOLEROS </span></p>
+                    <article className="sm:w-[50%] pl-2 pt-2 bg-slate-300">
+                        <p className="font-semibold text-[15px] text-left leading-5">Asociación de recicladores de oficio <span class="sm: text-[29px] font-bold">GOLEROS </span></p>
                     </article>
                 </section>
                 {/* contenedor del menu*/}
-                    <nav className=" w-[65%] flex  justify-center">
-                        <ul className="flex w-full pt-10 font-semibold text-xl">
-                            <li className="ml-12 mr-2"><a href="#">Servicios <span className="p-2">|</span></a></li>
+                     <nav className="sm: ml-3 w-[80%] flex  justify-center bg-slate-600">
+                        <ul className="sm: flex w-full pt-9 font-semibold text-lg bg-slate-300">
+                            <li className="sm: ml-3 mr-1"><a href="#">Servicios <span className="p-2">|</span></a></li>
                             <li className=""><a href="#">Nosotros</a></li>
-                            <li className=" ml-64"><a href="#">Iniciar sesión <span className="p-2">|</span></a></li>
+                            <li className="sm: ml-3"><a href="#">Iniciar sesión <span className="p-2">|</span></a></li>
                             <li><a href="#">Crear cuenta</a></li>
                         </ul>
-                    </nav>
+                    </nav>  
+
+
                 
             </header>
 
         </>
     )
 }
+
+
