7443dab609c954e14672674298e3e8d2607f4945
DanielSolarte23 <royersolarte22@gmail.com>
2024-06-20 16:07:17 -0500

    Proyect

--- src/pages/publicas/Home-Page.jsx
+++ src/pages/publicas/Home-Page.jsx
@@ -1,8 +1,8 @@
 import React from "react"
 /* Here we export the components that will contain the page*/
-import Header from "../components/Header-logo"
-import Bienvenida from "../components/Bienvenida-img"
-import Footer from "../components/Footer-icons"
+import Header from "../../components/Header-logo"
+import Bienvenida from "../../components/Bienvenida-img"
+import Footer from "../../components/Footer-icons"
 
 
 export default function HomePagina (){
