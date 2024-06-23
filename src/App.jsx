eb473d54eab1dfdbc7512fbd914ad57e1874ed0d
DanielSolarte23 <royersolarte22@gmail.com>
2024-06-23 12:43:09 -0500

    SobreNosotros,Formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react"
-import SobreNosotros from "./components/publicas/SobreNosotrosComponente";
-import Regisincidencia  from "./components/admins/Regisincidencia";
+
 export default function App(){
     return(
         <>
-        <SobreNosotros/>
-        <Regisincidencia/>
+
         </>
     );
 };
