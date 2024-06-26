690f5539acdafd02a11aba52ed1444dd6e70dafb
leonel <gun45780@gmail.com>
2024-06-26 13:44:10 -0500

    botones

---
+++ src/components/admins/BotonesCertificados.jsx
@@ -0,0 +1,16 @@
+export default function BotonesCertificado(){
+    return(
+        <section className="mt-10">
+        <div className="flex justify-center">
+            <button className="border border-black  bg-slate-500 items-center text-center w-52" type="submit">Registro de recoleccion <br /> en Fuentes</button>
+             </div>
+
+             <div className="flex justify-center mt-3">
+            <button className="border border-black items-center text-center w-52" type="submit">Formato registro operativo <br /> ruta S.O</button>
+             </div>
+             </section>
+
+
+    )
+
+}
