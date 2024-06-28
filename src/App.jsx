ba7b81537cde82a59c2e3d7bd0354dafa8251b78
DanielSolarte23 <royersolarte22@gmail.com>
2024-06-28 15:34:13 -0500

    Temporal Rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react"
+import Maps from "./components/admins/GestionRutasMap";
 
 export default function App(){
     return(
         <>
-
+        <Maps />
         </>
     );
 };
