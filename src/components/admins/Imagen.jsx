6b2e87b2bb17bb47c5391bd1384db6396c69fd17
leonel <gun45780@gmail.com>
2024-06-28 14:36:39 -0500

    imagenresuelta

--- src/components/admins/Imagen.jsx
+++ src/components/admins/Imagen.jsx
@@ -1,22 +1,27 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import images from  "../../images/imagenpersonas.png";
 
 export default function Imagen(){
     return(
-        <section>
-            <div className=" justify-center flex mt-6">
-            <img className="items-center" src="https://trello.com/1/cards/667c6aa1241afee21f352e5c/attachments/667c6ab17ca26c8bdcdc1828/previews/667c6ab27ca26c8bdcdc1880/download/imagennnuevaa.png" alt="imagen" />
-            </div>
-            <div className="justify-center flex">
-      <button className='w-40 h-8 hover:bg-transparent' type="submit" style={{ display: 'flex', alignItems: 'center' }}>
-        <span>Descargar</span>
-        <FontAwesomeIcon icon={faArrowDown} style={{ color: "#000000", marginLeft: '8px' }} />
-      </button>
-   <button className='ml-16 bg-red-600 w-40 h-8' type="submit">Borrar <FontAwesomeIcon icon={ faTrashAlt } style={{color: "#000000",}} /> </button>
-    </div>
-        
-        </section>
+        <section className="flex flex-col items-center justify-center h-screen">
+        <div className="px-4 w-10/12">
+            <img className="w-full mx-auto" src={images} alt="imagen" style={{ maxWidth: '100%', height: 'auto', width: '200%' }} />
+        </div>
+        <div className="flex mt-4">
+            <button className="w-32 h-8 text-center hover:bg-red-700 text-black  hover:border-red-700" type="submit">
+                Descargar <FontAwesomeIcon icon={faArrowDown} style={{ color: "#000000", marginLeft: '8px' }} />
+            </button>
+            <button className="ml-4 w-32 h-8 text-center bg-red-600 hover:bg-white text-black border border-black hover:border-red-700" type="submit">
+                Borrar <FontAwesomeIcon icon={faTrashAlt} style={{ color: "#000000", marginLeft: '8px' }} />
+            </button>
+        </div>
+    </section>
+    
+    
+
+
 
 
     )
