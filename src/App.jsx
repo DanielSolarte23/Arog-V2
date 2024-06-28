85771af46b1a2e5cf0d405abfdd30dd6b28bafcf
KevinPogo <kevinpenagosieb@gmail.com>
2024-06-28 15:39:02 -0500

    Cambios Joshua

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react"
+import Footer from "./components/publicas/Footer";
+
+
 
 export default function App(){
     return(
         <>
-
+           <Footer/>     
         </>
     );
 };
+
---
+++ src/components/publicas/Acerca.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Acerca = () => {
+    return(
+        <div>
+            <article className="font-lexend h-50 p-2.5">
+                <h1 className="text-4xl text-center">Sobre la empresa</h1>
+                <p className="text-1xl text-left">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id fugit libero minus, distinctio impedit nihil odio recusandae totam cupiditate obcaecati esse atque illo dicta mollitia necessitatibus repellendus optio nesciunt quos Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus eum ab a? Non sint atque, deleniti rerum nemo laudantium dolor impedit rem repudiandae sequi, ab odit ratione sapiente molestiae asperiores!?</p>
+            </article>
+        </div>
+    )
+}
+
+export  default Acerca
