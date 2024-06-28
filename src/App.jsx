1c77f129afaeb48a21fe321228133a9b33dcc62d
DanielSolarte23 <royersolarte22@gmail.com>
2024-06-27 21:24:11 -0500

    Ana Repair

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from "react"
-import HomePagina from "./pages/publicas/Home-Page";
-import Certificados from "./components/admins/Generar-Certificado";
-
 
 export default function App(){
     return(
         <>
-        <Certificados/>
+
         </>
     );
 };
