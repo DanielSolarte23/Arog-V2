433f3a9728c41629c15f7a8dcf4135774396d0a6
maria <mafeanaya22@gmail.com>
2024-07-08 23:12:56 -0500

    cuestionario

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react"
+import Cuestionario from "./components/admins/Cuestionario";    
 
 import Tareas from "./components/admins/Tareas";
 export default function App(){
     return(
         <>
-<Tareas/>
-
+<Cuestionario/>
         </>
     );
 }
---
+++ src/components/admins/Cuestionario.jsx
@@ -0,0 +1,27 @@
+export default function Cuestionario(){
+    return(
+
+        <article className="flex items-center justify-center h-screen w-screen ">
+        <div className="w-full h-full bg-white p-4 flex flex-col justify-center items-center">
+        <div className="text-center w-full mb-4 h-1/2 border  bg-gray-300 flex items-center justify-center">
+          <h2 className="text-lg font-medium">¿Preguntas?</h2>
+        </div>
+          <div className="grid grid-cols-2 gap-4 w-full px-4">
+            <button className="bg-gray-300 text-black py-2 px-4 rounded w-full">
+              A. Opción 1
+            </button>
+            <button className="bg-gray-300 text-black py-2 px-4 rounded w-full">
+              B. Opción 2
+            </button>
+            <button className="bg-gray-300 text-black py-2 px-4 rounded w-full">
+              C. Opción 3
+            </button>
+            <button className="bg-gray-300 text-black py-2 px-4 rounded w-full">
+              D. Opción 4
+            </button>
+          </div>
+        </div>
+      </article>
+    );
+  }
+  
